e98ac9ea12000ddbaa7c69edb1828c75580b89f0
mertcankiyak <iletisim@mertcankiyak.com>
2020-10-29 20:07:10 +0300

    Initial commit

---
+++ app/src/main/java/com/kodless/kotlinnavigationkullanimi/BirinciFragment.kt
@@ -0,0 +1,35 @@
+package com.kodless.kotlinnavigationkullanimi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_birinci.*
+
+class BirinciFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_birinci, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button1.setOnClickListener {
+            val action =
+                BirinciFragmentDirections.actionBirinciFragmentToİkinciFragment("Ben geldim")
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
